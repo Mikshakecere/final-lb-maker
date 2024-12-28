0d07986a90bd36296a9dc4b701e12c66b9c0e923
Mikshakecere <jngobus125@gmail.com>
2024-12-27 18:35:55 -0800

    Implemented proper time truncation

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ def seconds_to_minutes(seconds):
 def time_to_readable(time):
     time_seconds = int(str(time)[:-3])
     time_ms = str(time)[-3:]
+
+    # truncate the ms
+    time_ms = str(int(time_ms)//10)
+
     return str(seconds_to_minutes(time_seconds)) + "." + time_ms
 
 url = "https://data.ninjakiwi.com/"
 category = "btd6/races/"
-race_id = "Treet_Yourself_Extreme_Edition_m4ukr9d4/"
+race_id = requests.get(url + category).json()['body'][3]['id']
 tab = "leaderboard/"
 
-result = url + category + race_id + tab
+result = url + category + race_id + "/" + tab
 
 print("Result: " + result)
 
